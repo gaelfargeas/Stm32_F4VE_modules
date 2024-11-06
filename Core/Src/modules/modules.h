bc1fc978e79b2a74fa7e43338d3bb251401d8126
Gael_Fargeas <gael.faregas@gmail.com>
2024-11-06 14:33:14 +0100

    change name Module A -> Module AM2320

--- Core/Src/modules/modules.h
+++ Core/Src/modules/modules.h
@@ -8,20 +8,16 @@
 #ifndef SRC_MODULES_H_
 #define SRC_MODULES_H_
 
-#include "stdbool.h"
-
 #define MODULES_NUMBER 1
 
-
-
-
-/* Module A : AM2320 Module
+/* Module AM2320
  * com : I2C1
  * SDA	PB7
  * SCL	PB6
  */
-#define MODULE_A_ENABLE true
-#define MODULE_A_TIMER_MS 1000
-#define MODULE_A_ADDRESS 0xB8
+#define MODULE_AM2320_ENABLE 1
+#define MODULE_AM2320_TIMER_MS 1000
+#define MODULE_AM2320_ADDRESS 0xB8
+
 
 #endif /* SRC_MODULES_H_ */
