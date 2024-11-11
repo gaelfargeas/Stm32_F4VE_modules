09ac763143d9375ddda01d0fcbeaeb2aa5d0108a
Gael_Fargeas <gael.faregas@gmail.com>
2024-11-11 23:32:22 +0100

    create Template B : image background

---
+++ Core/Src/driver/LCD/LCD_templates/LCD_template_B.c
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file           : LCD_template_B.c
+  * @brief          : LCD template B functions.
+  ******************************************************************************
+  * history:
+  * ================================================================
+  * 2024-11-09 gael fargeas created
+  *
+  */
+#include "LCD_template_B.h"
+
+
+const LCD_template_typedef hLCD_template_B = {
+		.title_x = 10,
+		.title_y = 10,
+		.font_title = &Font24,
+		.text_x = 10,
+		.text_y = 50,
+		.font_text = &Font20,
+};
+
+/**
+* @brief display the template B
+* @param None.
+* @retval None
+*/
+void LCD_template_B_display(void)
+{
+ LCD_template_draw_image(&bmtemplate_B);
+}
---
+++ Core/Src/driver/LCD/LCD_templates/LCD_template_B.h
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file           : LCD_template_B.h
+  * @brief          : Header for LCD_template_B.c file.
+  ******************************************************************************
+  * history:
+  * ================================================================
+  * 2024-11-11 gael fargeas created
+  *
+  */
+#ifndef SRC_DRIVER_LCD_LCD_TEMPLATES_LCD_TEMPLATE_B_H_
+#define SRC_DRIVER_LCD_LCD_TEMPLATES_LCD_TEMPLATE_B_H_
+
+#include "LCD_template.h"
+
+extern const LCD_template_typedef hLCD_template_B;
+
+void LCD_template_B_display(void);
+
+#endif /* SRC_DRIVER_LCD_LCD_TEMPLATES_LCD_TEMPLATE_B_H_ */
